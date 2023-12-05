5224c59262c9db979bbea5be068bf743ad3a0375
Emre Cebi <emre@cebi.io>
2023-12-05 23:09:41 +0100

    Mac added

---
+++ macos/.emacs.d/init.el
@@ -0,0 +1,307 @@
+;;; init.el --- Load the full configuration -*- lexical-binding: t -*-
+;;; Commentary:
+
+;; Uga Buga
+
+;;; Code:
+
+;;Start speedup
+(let ((normal-gc-cons-threshold (* 20 1024 1024))
+      (init-gc-cons-threshold (* 128 1024 1024)))
+  (setq gc-cons-threshold init-gc-cons-threshold)
+  (add-hook 'emacs-startup-hook
+            (lambda () (setq gc-cons-threshold normal-gc-cons-threshold))))
+
+;; UI fixes
+(menu-bar-mode -1)
+(tool-bar-mode -1)
+(scroll-bar-mode -1)
+(tooltip-mode -1)
+(set-fringe-mode 10)
+(setq ns-pop-up-frames nil)
+(setq inhibit-startup-message t)
+(setq use-short-answers t)
+(setq confirm-nonexistent-file-or-buffer nil)
+(setq kill-buffer-query-functions
+  (remq 'process-kill-buffer-query-function
+        kill-buffer-query-functions))
+(setq history-length 25)
+(savehist-mode 1)
+
+
+;; Keys
+ (global-set-key (kbd "<escape>") 'keyboard-escape-quit )
+;; Mac spesific fixes
+(when (eq system-type 'darwin)
+  (setq native-comp-async-report-warnings-errors nil)
+  (setq mac-right-option-modifier 'super)
+  (add-to-list 'default-frame-alist '(ns-transparent-titlebar . t))
+  (add-to-list 'default-frame-alist '(ns-appearance . dark)))
+
+;; Clean folder
+(setq make-backup-files nil)
+
+;; Auto-save-mode doesn't create the path automatically!
+(make-directory (expand-file-name "tmp/auto-saves/" user-emacs-directory) t)
+
+(setq auto-save-list-file-prefix (expand-file-name "tmp/auto-saves/sessions/" user-emacs-directory)
+      auto-save-file-name-transforms `((".*" ,(expand-file-name "tmp/auto-saves/" user-emacs-directory) t)))
+
+(setq create-lockfiles nil)
+
+(setq user-emacs-directory (expand-file-name "~/.cache/emacs"))
+
+;;Font
+(set-face-attribute 'default nil :font "FiraCode Nerd Font Mono" :height 130)
+
+;;Column number
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+(dolist (mode '(org-mode-hook
+		term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook
+		dashboard-mode-hookx))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
+
+;;Package repos
+(require 'package)
+(add-to-list 'package-archives '("gnu"   . "https://elpa.gnu.org/packages/"))
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/"))
+(package-initialize)
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package-ensure)
+(setq use-package-always-ensure t)
+
+;;Loading custom files
+(add-to-list 'load-path "~/.emacs.d/custom")
+
+;;Keep init.el clean
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file 'noerror)
+
+;;Packages
+;; Get Shell Variables
+(use-package exec-path-from-shell)
+(when (memq window-system '(mac ns x))
+  (exec-path-from-shell-initialize))
+
+(use-package yasnippet)
+(yas-minor-mode)
+(use-package yasnippet-snippets)
+
+;; Projectile
+(use-package projectile
+  :init (projectile-mode +1)
+  :config
+  (setq projectile-project-search-path '("~/Documents/Projects"))
+  :bind
+  ("s-p" . projectile-command-map))
+
+;;doom-themes
+(use-package doom-themes
+  :config
+  (setq doom-themes-enable-bold t
+        doom-themes-enable-italic t)
+  (load-theme 'doom-city-lights t)
+  (doom-themes-visual-bell-config)
+  (doom-themes-neotree-config)
+  (doom-themes-treemacs-config)
+  (doom-themes-org-config))
+
+
+(use-package doom-modeline
+  :init (doom-modeline-mode 1))
+(setq doom-modeline-icon t)
+
+;;all-the-icons
+(use-package all-the-icons
+  :if (display-graphic-p))
+
+;;ivy
+(use-package ivy
+	:init (ivy-mode 1)
+	:custom
+	(setq ivy-use-virtual-buffers t
+				ivy-count-format "(%d/%d) "))
+
+(use-package ivy-hydra
+  :defer t
+  :after hydra)
+
+(global-set-key (kbd "C-s") 'swiper-isearch)
+(global-set-key (kbd "C-x C-f") 'counsel-find-file)
+(global-set-key (kbd "M-y") 'counsel-yank-pop)
+(global-set-key (kbd "<f1> f") 'counsel-describe-function)
+(global-set-key (kbd "<f1> v") 'counsel-describe-variable)
+(global-set-key (kbd "<f1> l") 'counsel-find-library)
+(global-set-key (kbd "<f2> i") 'counsel-info-lookup-symbol)
+(global-set-key (kbd "<f2> u") 'counsel-unicode-char)
+(global-set-key (kbd "<f2> j") 'counsel-set-variable)
+(global-set-key (kbd "C-x b") 'ivy-switch-buffer)
+(global-set-key (kbd "C-c v") 'ivy-push-view)
+(global-set-key (kbd "C-c V") 'ivy-pop-view)
+(global-set-key (kbd "C-c t") 'counsel-org-tag)
+
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)))
+
+;;Magit
+(use-package magit
+  :bind ("C-x g" . magit-status))
+
+;;Flycheck
+(use-package flycheck
+  :init (global-flycheck-mode)
+  :config
+  (setq flycheck-display-errors-function
+	#'flycheck-display-error-messages-unless-error-list)
+
+  (setq flycheck-indication-mode nil))
+
+(use-package flycheck-pos-tip
+  :ensure t
+  :after flycheck
+  :config
+  (flycheck-pos-tip-mode))
+
+;;Spellcheck
+(dolist (hook '(text-mode-hook))
+  (add-hook hook (lambda () (flyspell-mode 1))))
+
+(eval-after-load "flyspell"
+  '(progn
+     (define-key flyspell-mouse-map [down-mouse-3] #'flyspell-correct-word)
+     (define-key flyspell-mouse-map [mouse-3] #'undefined)))
+
+(defun flyspell-turkish ()
+  (interactive)
+  (ispell-change-dictionary "turkish")
+  (flyspell-buffer))
+
+(defun flyspell-english ()
+  (interactive)
+  (ispell-change-dictionary "default")
+  (flyspell-buffer))
+
+;; Completion
+(use-package company
+  :bind ("M-/" . company-complete-common-or-cycle) ;; overwritten by flyspell
+  :init (add-hook 'after-init-hook 'global-company-mode)
+  :config
+  (setq company-show-numbers            t
+	company-minimum-prefix-length   1
+	company-idle-delay              0.5
+	company-backends
+	'((company-files          ; files & directory
+	   company-keywords       ; keywords
+	   company-capf           ; what is this?
+	   company-yasnippet)
+	  (company-abbrev company-dabbrev))))
+
+(use-package company-box
+  :ensure t
+  :after company
+  :hook (company-mode . company-box-mode))
+;; Some text modes
+(use-package markdown-mode)
+(use-package yaml-mode)
+(use-package json-mode)
+
+(use-package which-key
+    :config
+    (which-key-mode))
+
+;; Python
+(use-package python-mode
+  :ensure nil
+  :custom
+  (python-shell-interpreter "python"))
+
+;; Org Mode
+(defun company-org-mode-hook ()
+  (setq-local company-backends
+	      '((company-dabbrev company-ispell company-yasnippet :separate)
+                company-files)))
+
+(use-package org
+  :hook
+  (org-mode . visual-line-mode)
+  (org-mode-hook . auto-revert-mod)
+  (org-mode-hook . my-org-mode-hook)
+  (org-mode-hook . company-org-mode-hook)
+  :config
+  (setq-default org-startup-indented t
+              org-pretty-entities t
+              org-use-sub-superscripts "{}"
+              org-hide-emphasis-markers t
+              org-startup-with-inline-images t
+              org-image-actual-width '(300))
+  (setq org-directory (concat (getenv "HOME") "/Documents/Notes/"))
+  (custom-set-faces
+   '(org-level-1 ((t (:inherit outline-1 :height 2.0))))
+   '(org-level-2 ((t (:inherit outline-2 :height 1.5))))
+   '(org-level-3 ((t (:inherit outline-3 :height 1.2))))
+   '(org-level-4 ((t (:inherit outline-4 :height 1.0))))
+   '(org-level-5 ((t (:inherit outline-5 :height 1.0))))
+   '(org-document-title ((t (:inherit default :height 2.0)))))
+  (advice-add 'org-refile :after 'org-save-all-org-buffers))
+
+;; Org image drag and drop
+(use-package org-download)
+(add-hook 'dired-mode-hook 'org-download-enable)
+
+;; Org distraction free writing
+(use-package olivetti)
+
+;; Org mode enphasis hider
+(use-package org-appear
+  :hook
+  (org-mode . org-appear-mode))
+
+;; Org bullets
+(use-package org-bullets
+  :after org
+  :hook
+  (org-mode . (lambda () (org-bullets-mode 1))))
+
+;; Better Inline LaTeX on Org
+(use-package org-fragtog
+  :after org
+  :custom
+  (org-startup-with-latex-preview t)
+  :hook
+  (org-mode . org-fragtog-mode)
+  :custom
+  (org-format-latex-options
+   (plist-put org-format-latex-options :scale 2)
+   (plist-put org-format-latex-options :foreground 'auto)
+   (plist-put org-format-latex-options :background 'auto)))
+
+;; Roam reseach
+(use-package org-roam
+  :after org
+  :init (setq org-roam-v2-ack t) ;; Acknowledge V2 upgrade
+  :custom
+  (org-roam-directory (file-truename org-directory))
+  :config
+  (org-roam-setup)
+  :bind (("C-c n f" . org-roam-node-find)
+         ("C-c n r" . org-roam-node-random)
+	 ("C-c n d" . org-roam-dailies-capture-today)
+         (:map org-mode-map
+               (("C-c n i" . org-roam-node-insert)
+                ("C-c n o" . org-id-get-create)
+                ("C-c n t" . org-roam-tag-add)
+                ("C-c n a" . org-roam-alias-add)
+                ("C-c n l" . org-roam-buffer-toggle)))))
+;;; init.el ends here
